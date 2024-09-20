30b42af3227eab07c1b3f2faa92e8a12530109df
sullis <github@seansullivan.com>
2024-09-20 15:31:45 -0700

    install iceberg

--- src/test/java/io/github/sullis/duckdb/playground/DuckdbDriverTest.java
+++ src/test/java/io/github/sullis/duckdb/playground/DuckdbDriverTest.java
@@ -1,6 +1,8 @@
 package io.github.sullis.duckdb.playground;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.duckdb.DuckDBConnection;
@@ -24,8 +26,20 @@ public class DuckdbDriverTest {
 
   @Test
   public void happyPath() throws Exception {
-    DuckDBConnection conn = (DuckDBConnection) DriverManager.getConnection("jdbc:duckdb:");
-    assertThat(conn).isNotNull();
-    assertThat(conn.isClosed()).isFalse();
+    try (DuckDBConnection conn = (DuckDBConnection) DriverManager.getConnection("jdbc:duckdb:")) {
+      assertThat(conn).isNotNull();
+      assertThat(conn.isClosed()).isFalse();
+      try (Statement statement = conn.createStatement()) {
+        // https://duckdb.org/docs/extensions/iceberg
+        statement.addBatch("install iceberg;");
+        statement.addBatch("load iceberg;");
+
+        // https://duckdb.org/docs/extensions/httpfs/overview.html
+        statement.addBatch("install httpfs;");
+        statement.addBatch("load httpfs;");
+
+        statement.executeBatch();
+      }
+    }
   }
 }
